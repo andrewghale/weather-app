e848fc4e78cd63c19e927281b7b2bbfe75a077c3
Andrew Hale <andyhalebmx@hotmail.com>
2020-02-24 10:01:19 +0000

    implementing type ahead feature

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const city = document.getElementById("cityInput")
-const search = document.getElementById("searchInput")
+const searchInput = document.getElementById("searchInput")
+const suggestions = document.querySelector(".suggestions")
 
-search.addEventListener('click', e => {
+
+searchInput.addEventListener('click', e => {
   e.preventDefault()
   getData(city.value)
 })
@@ -26,9 +28,20 @@ const epochToUtc = (time) => {
   return myDate
 }
 
-const displayTemp = (temp) => {
 
-}
+// const getCities = () => {
+//   const citiesUrl = "https://gist.githubusercontent.com/andrewghale/e2dad9afd1aaf95bd16dec88c7b8a1bb/raw/5aa8b1e321458f1775329cc6adb6e2ad0fc39d4a/citiesUK"
+
+//   fetch(citiesUrl)
+//     .then( (data) => data.json())
+//     .then( (data) => console.log(data))
+// }
+
+
+
+// const displayTemp = (temp) => {
+
+// }
 
 // var dateString = 'Mon Jan 12 00:00:00 GMT 2015';
 // dateString = new Date(dateString).toUTCString();
@@ -50,3 +63,57 @@ const generateHtml = (data) => {
   const outputDiv = document.getElementById('output')
   outputDiv.innerHTML = html
 }
+
+
+///////////////////
+////////////////////
+//////////////////
+
+// use below function on this url
+// https://en.wikipedia.org/wiki/List_of_cities_in_the_United_Kingdom#List_of_cities
+
+const mainArr = () => {
+  let cities = []
+  const category = document.querySelector(".wikitable")
+  const body = Array.from(category.querySelectorAll("tbody tr"))
+  body.forEach((element) => {
+    const link = Array.from(element.querySelectorAll("a"))
+    const city = link[0].text
+    cities.push({"city":city})
+  })
+  console.log(JSON.stringify(cities))
+}
+
+// console.log(JSON.stringify(uniqueNames))
+
+////////////////////
+/////////////////////
+//////////////////
+
+// const cities = []
+// const fetchTest = () => {
+//   const url = "https://gist.githubusercontent.com/andrewghale/c972506d41833c4dd8db51d761e1b3f6/raw/838d146ef12c96ae534ce6fff7b0892667e67f60/citiesUK.json"
+//   fetch(url)
+//     .then( blob => blob.json() )
+//     .then( data => cities.push(...data))
+//   return cities
+//   }
+//   console.log(cities)
+
+const cities = [{"city":"Aberdeen"},{"city":"Armagh"},{"city":"Bangor"},{"city":"Bath"},{"city":"Belfast"},{"city":"Birmingham"},{"city":"Bradford"},{"city":"Brighton & Hove"},{"city":"Bristol"},{"city":"Cambridge"},{"city":"Canterbury"},{"city":"Cardiff"},{"city":"Carlisle"},{"city":"Chelmsford"},{"city":"Chester"},{"city":"Chichester"},{"city":"Coventry"},{"city":"Derby"},{"city":"Derry"},{"city":"Dundee"},{"city":"Durham"},{"city":"Edinburgh"},{"city":"Ely"},{"city":"Exeter"},{"city":"Glasgow"},{"city":"Gloucester"},{"city":"Hereford"},{"city":"Inverness"},{"city":"Kingston upon Hull"},{"city":"Lancaster"},{"city":"Leeds"},{"city":"Leicester"},{"city":"Lichfield"},{"city":"Lincoln"},{"city":"Lisburn"},{"city":"Liverpool"},{"city":"London"},{"city":"Manchester"},{"city":"Newcastle upon Tyne"},{"city":"Newport"},{"city":"Newry"},{"city":"Norwich"},{"city":"Nottingham"},{"city":"Oxford"},{"city":"Perth"},{"city":"Peterborough"},{"city":"Plymouth"},{"city":"Portsmouth"},{"city":"Preston"},{"city":"Ripon"},{"city":"St Albans"},{"city":"St Asaph"},{"city":"St Davids"},{"city":"Salford"},{"city":"Salisbury"},{"city":"Sheffield"},{"city":"Southampton"},{"city":"Stirling"},{"city":"Stoke-on-Trent"},{"city":"Sunderland"},{"city":"Swansea"},{"city":"Truro"},{"city":"Wakefield"},{"city":"Wells"},{"city":"Westminster"},{"city":"Winchester"},{"city":"Wolverhampton"},{"city":"Worcester"},{"city":"York"}]
+
+const findMatches = (wordToMatch, cities) => {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi')
+    return place.city.match(regex)
+  })
+}
+
+function displayMatches(){
+  // console.log(this.value)
+  const matchArray = findMatches(this.value, cities)
+  console.log(matchArray)
+}
+
+city.addEventListener('change', displayMatches)
+city.addEventListener('keyup', displayMatches)
