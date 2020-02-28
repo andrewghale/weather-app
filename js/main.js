78ab308022e364ae81df43bafbda8170549761ef
Andrew Hale <andyhalebmx@hotmail.com>
2020-02-28 09:09:25 +0000

    forecast js

--- js/main.js
+++ js/main.js
@@ -49,35 +49,36 @@ const generateHtml = (data) => {
   const { weather, main, sys, name } = data;
   const html = `
   <div class="output-container">
-    <h3>${name}</h3>
+    <h1>${name}</h1>
     <p>${weather[0].description}</p>
-    <p>Temperature: ${main.temp.toFixed(1)}&#176;C</p>
+    <p>${main.temp.toFixed(1)}&#176;C</p>
     <p>Sunrise: ${epochToUtc(sys.sunrise)}</p>
     <p>Sunset: ${epochToUtc(sys.sunset)}</p>
   </div>
   `
   const outputDiv = document.getElementById('output')
   outputDiv.innerHTML = html
+  init()
 }
 
 const generateForecastHtml = (data) => {
   let temps = ''
-  console.log(data)
   data.list.forEach((el) => {
+    const { dt, main, wind } = el
     let tempString =
-    `<li class="three-hour" id="${el.dt}">
+    `<li class="three-hour" id="${dt}">
       <div class="card">
           <div class="time">
             <span class="bold">${
-              JSON.stringify(epochToTime(el.dt)).padStart(2, '0')}
+              JSON.stringify(epochToTime(dt)).padStart(2, '0')}
             </span>00
           </div>
         <div class="temp">
-          ${JSON.stringify(Math.round(el.main["temp"]))}&#176;
+          ${JSON.stringify(Math.round(main["temp"]))}&#176;
         </div>
         <div class="wind">
           <img src="../img/wind.svg" alt="wind">
-          ${JSON.stringify(Math.round(el.wind["speed"]))}
+          ${JSON.stringify(Math.round(wind["speed"]))}
         </div>
       </div>
       <div class="card-large">
@@ -106,10 +107,8 @@ const generateForecastHtml = (data) => {
   `
   const outputForecastDiv = document.getElementById('output-forecast')
   outputForecastDiv.innerHTML = html
-  // init()
+  init()
 }
-
-const cities = [{"city":"Aberdeen"},{"city":"Armagh"},{"city":"Bangor"},{"city":"Bath"},{"city":"Belfast"},{"city":"Birmingham"},{"city":"Bradford"},{"city":"Brighton & Hove"},{"city":"Bristol"},{"city":"Cambridge"},{"city":"Canterbury"},{"city":"Cardiff"},{"city":"Carlisle"},{"city":"Chelmsford"},{"city":"Chester"},{"city":"Chichester"},{"city":"Coventry"},{"city":"Derby"},{"city":"Derry"},{"city":"Dundee"},{"city":"Durham"},{"city":"Edinburgh"},{"city":"Ely"},{"city":"Exeter"},{"city":"Glasgow"},{"city":"Gloucester"},{"city":"Hereford"},{"city":"Inverness"},{"city":"Kingston upon Hull"},{"city":"Lancaster"},{"city":"Leeds"},{"city":"Leicester"},{"city":"Lichfield"},{"city":"Lincoln"},{"city":"Lisburn"},{"city":"Liverpool"},{"city":"London"},{"city":"Manchester"},{"city":"Newcastle upon Tyne"},{"city":"Newport"},{"city":"Newry"},{"city":"Norwich"},{"city":"Nottingham"},{"city":"Oxford"},{"city":"Perth"},{"city":"Peterborough"},{"city":"Plymouth"},{"city":"Portsmouth"},{"city":"Preston"},{"city":"Ripon"},{"city":"St Albans"},{"city":"St Asaph"},{"city":"St Davids"},{"city":"Salford"},{"city":"Salisbury"},{"city":"Sheffield"},{"city":"Southampton"},{"city":"Stirling"},{"city":"Stoke-on-Trent"},{"city":"Sunderland"},{"city":"Swansea"},{"city":"Truro"},{"city":"Wakefield"},{"city":"Wells"},{"city":"Westminster"},{"city":"Winchester"},{"city":"Wolverhampton"},{"city":"Worcester"},{"city":"York"}]
 
 const findMatches = (wordToMatch, cities) => {
   return cities.filter(place => {
@@ -140,8 +139,9 @@ const capitalizeFirst = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-  let allPanels = $('.card-large').hide();
 
+const init = () => {
+  let allPanels = $('.card-large').hide();
   $('.three-hour').click(function() {
     let cardLarge = document.querySelector(".card-large")
     allPanels.hide()
@@ -149,6 +149,7 @@ const capitalizeFirst = (string) => {
       .children(cardLarge)
       .show()
   });
+}
 
 
 
